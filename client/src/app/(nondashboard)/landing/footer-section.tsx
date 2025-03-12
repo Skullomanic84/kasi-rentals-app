96b7d13dff4eb9917bcb571e6b0656a8c77283d8
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2025-03-12 22:31:49 +0200

    added react-icon to the packages and implemented the footer section and imported it to the landing page

--- client/src/app/(nondashboard)/landing/footer-section.tsx
+++ client/src/app/(nondashboard)/landing/footer-section.tsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import {
+  CiFacebook,
+  CiInstagram,
+  CiTwitter,
+  CiLinkedin,
+  CiYoutube,
+} from "react-icons/ci";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="border-t border-gray-200 py-20">
+      <div className="max-w-4xl mx-auto px-6 sm:px-8">
+        <div className="flex flex-col md:flex-col justify-center items-center">
+          {/* logo title */}
+          <div className="mb-4">
+            <Link href="/" className="text-xl font-black" scroll={false}>
+              KASIRENTALS
+            </Link>
+          </div>
 
-export default Footer
+          {/* link section */}
+          <nav className="mb-4">
+            <ul className="flex space-x-6">
+              <li>
+                <Link href="/about">About Us</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact Us</Link>
+              </li>
+              <li>
+                <Link href="/faq">FAQ</Link>
+              </li>
+              <li>
+                <Link href="/terms">Terms & Conditions</Link>
+              </li>
+              <li>
+                <Link href="/privacy">Privacy</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* icon links */}
+
+          <div className="flex space-x-4 mb-4">
+            <a
+              href="#"
+              aria-label="Facebook"
+              className="hover:text-primary-600"
+            >
+              <CiFacebook className="h-6 w-6" />
+            </a>
+            <a
+              href="#"
+              aria-label="Facebook"
+              className="hover:text-primary-600"
+            >
+              <CiInstagram className="h-6 w-6" />
+            </a>
+            <a
+              href="#"
+              aria-label="Facebook"
+              className="hover:text-primary-600"
+            >
+              <CiTwitter className="h-6 w-6" />
+            </a>
+            <a
+              href="#"
+              aria-label="Facebook"
+              className="hover:text-primary-600"
+            >
+              <CiLinkedin className="h-6 w-6" />
+            </a>
+            <a
+              href="#"
+              aria-label="Facebook"
+              className="hover:text-primary-600"
+            >
+              <CiYoutube className="h-6 w-6" />
+            </a>
+          </div>
+        </div>
+
+        {/* copy right */}
+        <div className="mt-4 text-center text-gray-500 text-sm flex justify-center space-x-4">
+          <span>&copy; KASIrental. All Rights Reserved.</span>
+          <Link href="/privacy">Privacy Policy</Link>
+          <Link href="/terms-of-service">Terms of Service</Link>
+          <Link href="/cookie-policy">Cookie Policy</Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
